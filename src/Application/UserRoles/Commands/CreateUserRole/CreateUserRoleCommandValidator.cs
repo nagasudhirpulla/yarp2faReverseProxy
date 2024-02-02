3c59d1962e3a5487dd3628ade38a9c190d6d3417
Nagasudhir Pulla <Nagasudhir@wrldc.local>
2024-02-02 15:05:06 +0530

    create role page done

---
+++ src/Application/UserRoles/Commands/CreateUserRole/CreateUserRoleCommandValidator.cs
@@ -0,0 +1,14 @@
+﻿using Application.Users.Commands.CreateUser;
+using Application.Users;
+using Core.Entities;
+using FluentValidation;
+
+namespace Application.UserRoles.Commands.CreateUserRole;
+
+public class CreateUserRoleCommandValidator : AbstractValidator<CreateUserRoleCommand>
+{
+    public CreateUserRoleCommandValidator()
+    {
+        RuleFor(x => x.Name).NotEmpty();
+    }
+}
